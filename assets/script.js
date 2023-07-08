b560bf9b71f4e34cfc6464e1072f3720c31e5086
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-07-07 21:19:45 -0500

    added a view scores button that shows the previos high scores

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,140 @@
 var questionEl = $('#question');
 var timeEl = $('#time');
-//const questionEl = document.getElementById("question");
+var card1El = $('#card1');
+var card2El = $('#card2');
 var choiceAEl =  $('#choiceA');
 var choiceBEl =  $('#choiceB');
 var choiceCEl =  $('#choiceC');
 var choice = document.getElementById("choice");
-var score = 0;
+var score = localStorage.getItem('score') || 0;
 var currentQuestion = 0;
 
-var count = localStorage.getItem("count");
 
 function main(){
     setTime();
     renderQuestion();
 }
 
-console.log("score: " +score)
-choiceAEl.on('click', function () {
-    if(myQuestions[currentQuestion].correctAnswer === "a"){
-        score++;
-    }
-    console.log("score: " +score);
-    if(currentQuestion < myQuestions.length-1){
-        currentQuestion++;
-    }
-    else{
-        newScore();
-        return;
-    }
-    renderQuestion();
-});
-choiceBEl.on('click', function () {
-    if(myQuestions[currentQuestion].correctAnswer === "b"){
-        score++;
-    }
-    console.log("score: " +score);
-    if(currentQuestion < myQuestions.length-1){
-        currentQuestion++;
-    }
-    renderQuestion();
-});
-choiceCEl.on('click', function () {
-    if(myQuestions[currentQuestion].correctAnswer === "c"){
-        score++;
-    }
-    console.log("score: " +score);
-    if(currentQuestion < myQuestions.length-1){
-        currentQuestion++;
-    }
-    renderQuestion();
+
+console.log("score: " + score);
+choiceAEl.on('click', function() {
+  if (myQuestions[currentQuestion].correctAnswer === "a") {
+    score++;
+  }
+  console.log("score: " + score);
+  if (currentQuestion < myQuestions.length - 1) {
+    currentQuestion++;
+  } else {
+    newScore();
+    return;
+  }
+  renderQuestion();
 });
 
+choiceBEl.on('click', function() {
+  if (myQuestions[currentQuestion].correctAnswer === "b") {
+    score++;
+  }
+  console.log("score: " + score);
+  if (currentQuestion < myQuestions.length - 1) {
+    currentQuestion++;
+  } else {
+    newScore();
+    return;
+  }
+  renderQuestion();
+});
 
-// choice.addEventListener("click", function() {
-//   if (count < 24) {
-//     score++;
-    
-//     localStorage.setItem("score", score);
-//   }
-// });
+choiceCEl.on('click', function() {
+  if (myQuestions[currentQuestion].correctAnswer === "c") {
+    score++;
+  }
+  console.log("score: " + score);
+  if (currentQuestion < myQuestions.length - 1) {
+    currentQuestion++;
+  } else {
+    newScore();
+    return;
+  }
+  renderQuestion();
+});
 
-// Selects element by class
-//var timeEl = document.querySelector(".time");
+function newScore() {
+  card1El.text("Game over! Your score is: " + score);
 
-// Selects element by id
-var mainEl = document.getElementById("main");
+  // Create the HTML form
+  var formHtml = '<form id="nameForm">';
+  formHtml += '<label for="name">Enter your name:</label>';
+  formHtml += '<input type="text" id="name" name="name">';
+  formHtml += '<input type="submit" value="Submit">';
+  formHtml += '</form>';
+
+  // Append the form to card2El
+  card2El.html(formHtml);
+
+  // Add a submit event listener to the form
+  $('#nameForm').on('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var playerName = $('#name').val(); // Get the entered name
+
+    // Retrieve existing high scores from localStorage
+    var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+    // Add the current score to the high scores array
+    highScores.push({
+      playerName: playerName,
+      score: score
+    });
+
+    // Store the updated high scores in localStorage
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    // Reset the score to 0
+    score = 0;
+
+    // Reset the form and reload the page
+    $(this)[0].reset();
+    location.reload();
+  });
+}
+
+
+// Function to display high scores
+function viewHighScores() {
+  // Retrieve high scores and player names from localStorage
+  var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+  // Sort high scores in descending order
+  highScores.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  // Create HTML for displaying scores
+  var scoresHtml = '<div class="high-scores">';
+  scoresHtml += '<h2>High Scores</h2>';
+  if (highScores.length > 0) {
+    scoresHtml += '<ul>';
+    for (var i = 0; i < highScores.length; i++) {
+      var playerName = highScores[i].playerName;
+      var score = highScores[i].score;
+      scoresHtml += '<li>' + playerName + ': ' + score + '</li>';
+    }
+    scoresHtml += '</ul>';
+  } else {
+    scoresHtml += '<p>No high scores available.</p>';
+  }
+  scoresHtml += '</div>';
+
+  // Display scores in the scoresContainer element
+  var scoresContainer = document.getElementById('scoresContainer');
+  scoresContainer.innerHTML = scoresHtml;
+}
+
+// Add event listener to the View Scores button
+var viewScoresBtn = document.getElementById('viewScoresBtn');
+viewScoresBtn.addEventListener('click', viewHighScores);
+viewScoresBtn.classList.add('score-button');
 
 var secondsLeft = 60;
 
@@ -77,15 +147,13 @@ function setTime() {
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
-      // Calls function to create and append image
+      // Calls function to show score
       newScore();
     }
 
   }, 1000);
 }
-function newScore(){
-    
-}
+
 
 function renderQuestion(){
         questionEl.text(myQuestions[currentQuestion].question);
@@ -113,16 +181,6 @@ const myQuestions = [
       },
       correctAnswer: "c"
     },
-    // {
-    //   question: "Which tool can you use to ensure code quality?",
-    //   answers: {
-    //     a: "Angular",
-    //     b: "jQuery",
-    //     c: "RequireJS",
-    //     d: "ESLint"
-    //   },
-    //   correctAnswer: "d"
-    // }
   ];
 
 main();
